fefec494489e85670a545214564853ace0429e94
David Fenneran <148485725+erraticandunplanned@users.noreply.github.com>
2026-08-26 22:11:00 -0400

    closer to hook functionality
    LunarTowline hook can grab and pull, but there is currently no way to "disconnect" the hook afterwards.

--- 2_spells/actives/LunarTowline/familiar/lunartowline_hook.gd
+++ 2_spells/actives/LunarTowline/familiar/lunartowline_hook.gd
@@ -9,13 +9,11 @@ const GRAVITY: float = 0.25
 var Velocity: Vector3
 
 var hooked_id : int = 0
-var HOOK_POS_OWNER : Vector3
-var HOOK_POS_TARGET : Vector3
+var HOOK_OWNER  : Node3D ## can be either a Player or a Dummy
+var HOOK_TARGET : Node3D ## can be either a Player or a Dummy
 
 var mesh_instance := MeshInstance3D.new()
 
-# we use _init() and reduce_to_byte_array() to ensure all necessary info is present
-# before sending over the network.
 func _init(owner_id: int, projectile_pos: Vector3, projectile_velocity: Vector3) -> void:
    super(owner_id, MAX_LIFE_TIME)
    name = str(owner_id) + "__lunar_towline_hook__" + str(randi())
@@ -36,7 +34,7 @@ func _init(owner_id: int, projectile_pos: Vector3, projectile_velocity: Vector3)
    collisionshape.shape.radius = 4.0
    
    area.collision_layer = 8
-   area.collision_mask = 4
+   area.set_collision_mask_value(3, true)
    area.body_entered.connect(_on_body_entered)
 
 func reduce_to_byte_array() -> PackedByteArray: 
@@ -57,15 +55,14 @@ static func create_from_byte_array(owner_id: int, data: PackedByteArray) -> Luna
    return LunarTowline_Hook.new(owner_id, new_position, new_velocity) 
 
 func _physics_process(delta: float) -> void:
-   if hooked_id != 0:
-      pass
-   else:
+   ## MOVE ACCORDING TO VELOCITY IF NOT HOOKED
+   if hooked_id == 0:
       if Velocity.length_squared() > 0.001:
          var dir := Velocity.normalized()
          var up := Vector3.UP
          if abs(dir.dot(up)) > 0.999:
             up = Vector3.FORWARD
-
+         
          var target_basis := Basis.looking_at(dir, up)
          var target_quat := target_basis.get_rotation_quaternion()
          var current_quat := global_transform.basis.get_rotation_quaternion()
@@ -74,13 +71,61 @@ func _physics_process(delta: float) -> void:
          
       position   +=  delta*SPEED*Velocity
       Velocity.y += -delta*GRAVITY
+   ## REMAIN WITH THE HOOKED CHARACTER IF THERE IS ONE
+   else:
+      var pos0 = (HOOK_TARGET.position + Vector3.UP) if HOOK_TARGET else Vector3.ZERO
+      var pos1 = (HOOK_OWNER.position + Vector3.UP) if HOOK_OWNER else Vector3.ZERO
+      _draw_line(pos0, pos1)
+
+func _draw_line(pos0 : Vector3, pos1 : Vector3, color : Color = Color.WHITE):
+   var immediate_mesh := ImmediateMesh.new()
+   mesh_instance.mesh = immediate_mesh
+   mesh_instance.cast_shadow = GeometryInstance3D.SHADOW_CASTING_SETTING_OFF
+   var material = ORMMaterial3D.new()
+   material.shading_mode = BaseMaterial3D.SHADING_MODE_UNSHADED
+   material.albedo_color = color
+   immediate_mesh.surface_begin(Mesh.PRIMITIVE_LINES, material)
+   immediate_mesh.surface_add_vertex(pos0)
+   immediate_mesh.surface_add_vertex(pos1)
+   immediate_mesh.surface_end()
 
 #func _process(delta: float) -> void:
    #super(delta) # NOTE: keep this line if adding custom _process(). kills self at end of life.
 
 func _on_body_entered(body : Node3D):
    if hooked_id != 0: return
-   if body is Player or body is Dummy:
-      if body.NETWORK_ID == OwnerID: return
-      hooked_id = body.NETWORK_ID
-      print("hooked ", hooked_id)
+   var check_body = body if body is Player else body.get_parent() if body.get_parent() is Dummy else null
+   if check_body is Player or check_body is Dummy:
+      if check_body.NETWORK_ID == OwnerID: return
+      hooked_id = check_body.NETWORK_ID
+      find_hook_owner()
+      find_hook_target()
+      if OwnerID == get_parent().get_parent().MPM.get_local_player_id(): send_hook_id_to_owner()
+      position = Vector3.ZERO
+      rotation = Vector3.ZERO
+func find_hook_owner():
+   if OwnerID == get_parent().get_parent().MPM.get_local_player_id():
+      HOOK_OWNER = get_parent().get_parent().find_child("Player") as Player
+   elif SettingsManager.peer_settings.has(OwnerID):
+      HOOK_OWNER = SettingsManager.peer_settings.get(OwnerID).dummy as Dummy
+   else:
+      printerr("owner id doesn't exist in peer settings. see lunartowline_hood.gd")
+func find_hook_target():
+   if hooked_id == get_parent().get_parent().MPM.get_local_player_id():
+      HOOK_TARGET = get_parent().get_parent().find_child("Player") as Player
+   elif SettingsManager.peer_settings.has(hooked_id):
+      HOOK_TARGET = SettingsManager.peer_settings.get(hooked_id).dummy as Dummy
+   else:
+      printerr("hooked player id doesn't exist in peer settings. see lunartowline_hood.gd")
+
+## called after a player has been hooked, and only if this familiar is running on the familiar's owner's computer
+func send_hook_id_to_owner():
+   for i in range(ThePlayer.SpellBook.ActiveSlots):
+      var test_spell = ThePlayer.SpellBook.ActiveSpells[i]
+      if test_spell is LunarTowlineSpell and test_spell.CurrentState == test_spell.States.Cast:
+         test_spell.recieve_target_id(hooked_id)
+func get_state_from_spell():
+   for i in range(ThePlayer.SpellBook.ActiveSlots):
+      var test_spell = ThePlayer.SpellBook.ActiveSpells[i]
+      if test_spell is LunarTowlineSpell and ( test_spell.CurrentState == test_spell.States.Reeling or test_spell.CurrentState == test_spell.States.None ):
+         self.queue_free()
--- 2_spells/actives/LunarTowline/lunartowline_script.gd
+++ 2_spells/actives/LunarTowline/lunartowline_script.gd
@@ -1,13 +1,18 @@
 extends ActiveSpell
 class_name LunarTowlineSpell
 
+const SPELL_FORCE : float = -10
+
 enum States {None, Charging, Cast, Hold, Reeling}
 enum FamiliarIDs {Hook}
 
 var cast_power : float = 0.0
 
-#func on_state_changed(new_state: int) -> void:
-   #super(new_state) # NOTE: keep this line if adding custom state change logic
+var target_id : int = 0
+
+func on_state_changed(new_state: int) -> void:
+   super(new_state)
+   if new_state == States.Hold: print("captured ", target_id)
 
 func _init() -> void: super(SpellData.ActiveSpellIDs.LunarTowline)
 
@@ -16,6 +21,9 @@ func _on_activate(_activator: Player) -> void:
       CurrentState = States.Reeling
       TimeSinceActivation = 0.0
       cast_power = 0.0
+      if target_id != 0:
+         send_damage()
+         target_id = 0
       print("reeled the line in")
    elif _can_activate():
       CurrentState = States.Charging
@@ -41,3 +49,23 @@ func _cast(activator : Player):
    var hook_velocity: Vector3 = player_look_dir.normalized() * cast_power
    var temp_familiar := LunarTowline_Hook.new(activator.NETWORK_ID, hook_pos, hook_velocity)
    activator.spawn_familiar(true, SpellData.ActiveSpellIDs.LunarTowline, FamiliarIDs.Hook, temp_familiar.reduce_to_byte_array())
+
+func recieve_target_id(id: int):
+   target_id = id
+   CurrentState = States.Hold
+
+func send_damage():
+   if not SettingsManager.peer_settings.has(target_id): return
+   var enemy : Dummy = SettingsManager.peer_settings.get(target_id).dummy
+   
+   var stat_influenced_force  : float = SPELL_FORCE  * SpellData.get_influenced_stat(SpellData.StatTypes.FORCE, SettingsManager.match_settings.PLAYER_BASE_STATS[SpellData.StatTypes.FORCE], Player_Self.SpellBook.get_stat(SpellData.StatTypes.FORCE))
+   
+   var new_damage_package = DamagePackage.new()
+   new_damage_package.id_from = Player_Self.NETWORK_ID
+   new_damage_package.id_to = enemy.NETWORK_ID
+   new_damage_package.location_source = Player_Self.global_position
+   new_damage_package.location_receipt = enemy.global_position
+   new_damage_package.amount = 0.0
+   new_damage_package.type = DamagePackage.DamageType.IMPACT
+   new_damage_package.force = stat_influenced_force
+   Player_Self.send_damage_package(new_damage_package)
